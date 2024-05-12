3013d4ed85c60e0fcf59bdc1b12738d9a96defd7
Abhishekshri15 <abhishekshri15@gmail.com>
2024-05-12 16:46:51 +0530

    Speach to text Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Speach from './Speach';
+
+function App() {
+  return (
+    <div>
+      <Speach/>
+    </div>
+  );
+}
+
+export default App;
